5b1bfc7b52d20eb9ed37dd090da69293eb104f62
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-09 07:02:38 +0900

    Array.indexOf test

---
+++ array-indexof-test.js
@@ -0,0 +1,27 @@
+// test.js
+'use strict';
+if (typeof describe === 'undefined')
+  return console.log('mocha required...');
+var assert = require('assert');
+describe('Array', function (){
+  describe('#indexOf()', function (){
+    it('should return -1 when the value is not present', function (){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+    it('should return -1 when the value is not present', function (){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+  describe('#indexOf()', function (){
+    it('should return -1 when the value is not present', function (){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+    it('should return -1 when the value is not present', function (){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(1, [1,2,3].indexOf(0));
+    });
+  });
+});
